b09d9d407ded7d65b2c1a5f6c8e0ee29c2b40e4f
Rohit Magar <iamrohitmagar@gmail.com>
2023-02-27 20:25:07 +0530

    Add files via upload

---
+++ tik-ta-toe/src/App.js
@@ -0,0 +1,6 @@
+
+import Game from './components/Game';
+
+const App = () => <Game />;
+
+export default App;
---
+++ tik-ta-toe/src/components/Square.js
@@ -0,0 +1,16 @@
+const style = {
+    background: 'lightblue',
+    border: '2px solid darkblue',
+    fontSize: '30px',
+    fontWeight: '800',
+    cursor: 'pointer',
+    outline: 'none'
+};
+
+const Square = ({ value, onClick }) => (
+    <button style={style} onClick={onClick}>
+        {value}
+    </button>
+);
+
+export default Square;
